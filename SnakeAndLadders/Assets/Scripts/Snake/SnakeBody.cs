6ab76a9b4bd85e32a51a21d91942dd4be41952df
Kent Dave Catipay <kpcatipay@up.edu.ph>
2024-12-08 14:50:49 +0800

    snake body

---
+++ SnakeAndLadders/Assets/Scripts/Snake/SnakeBody.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SnakeBody : MonoBehaviour
+{
+    public int length = 10;
+    public LineRenderer body;
+    public Transform snakehead;
+    private Vector3[] segmentPositions;
+    private Vector3[] segmentVelocities;
+    public float smoothTime = 0.1f;
+    public float gravity = -9.81f;
+    public float segmentDistance = 1f; // Distance between segments
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body.positionCount = length;
+        segmentPositions = new Vector3[length];
+        segmentVelocities = new Vector3[length];
+        segmentPositions[0] = this.snakehead.position;
+        body.SetPosition(0, this.snakehead.position);
+
+        for (int i = 1; i < length; i++)
+        {
+            segmentPositions[i] = segmentPositions[i - 1] - snakehead.forward * segmentDistance;
+            body.SetPosition(i, segmentPositions[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        segmentPositions[0] = this.snakehead.position;
+        body.SetPosition(0, this.snakehead.position);
+
+        for (int i = 1; i < length; i++)
+        {
+            // Apply gravity to the segment velocity
+            segmentVelocities[i].y += gravity * Time.deltaTime;
+
+            // Smoothly move the segment towards the previous segment's position
+            segmentPositions[i] = Vector3.SmoothDamp(segmentPositions[i], segmentPositions[i - 1], ref segmentVelocities[i], smoothTime);
+
+            // Update the LineRenderer position
+            body.SetPosition(i, segmentPositions[i]);
+        }
+    }
+}
+
